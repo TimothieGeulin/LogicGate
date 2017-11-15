b848b93a2b5b1b1ea25ce4f4dc2057af21d3107b
Surdjak <n64player@live.com>
2017-11-15 17:02:54 +0100

    On repart de 0, avec des trucs plus simples

--- main.py
+++ main.py
@@ -1,108 +1,31 @@
-from time import time
+import kivy
+kivy.require('1.1.1')
+
 from kivy.app import App
-from os.path import dirname, join
-from kivy.lang import Builder
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, StringProperty, BooleanProperty, ListProperty, ObjectProperty
-from kivy.clock import Clock
-from kivy.animation import Animation
-from kivy.uix.screenmanager import Screen
-from kivy.graphics.instructions import Canvas, Instruction
-from kivy.graphics import Line, Color, Rectangle, Ellipse
+from kivy.properties import NumericProperty, ReferenceListProperty,\
+    ObjectProperty
 from kivy.vector import Vector
+from kivy.clock import Clock
 
-class GameScreen(Screen):
-    fullscreen = BooleanProperty(False)
 
-    def add_widget(self, *args):
-        if 'content' in self.ids:
-            return self.ids.content.add_widget(*args)
-        return super(GameScreen, self).add_widget(*args)
-        
-class Gate(Widget):
-	entryPoints = ListProperty(0)
-	
-	def on_touch_move(self, touch):
-		print("TOUCH DEDANS\n")
-		if (touch.x >= self.x) & (touch.y >= self.y) & (touch.x <= self.x + self.size.x) & (touch.y <= self.y + self.size.y):
-			self.y = touch.y+75
-			self.x = touch.x+200
- 
+class PongPaddle(Widget):
+    score = NumericProperty(0)
 
-class LogicGateApp(App):
-	
-	index = NumericProperty(-1)
-	current_title = StringProperty()
-	time = NumericProperty(0)
-	show_sourcecode = BooleanProperty(False)
-	sourcecode = StringProperty()
-	screen_names = ListProperty([])
-	hierarchy = ListProperty([])
-	gate = ObjectProperty(None)
-    
-	def build(self):
-		self.title = 'Logic Gate'
-		Clock.schedule_interval(self._update_clock, 1 / 60.)
-		self.screens = {}
-		self.available_screens = (['MainMenu', 'LevelSelect', 'Level1'])
-		self.screen_names = self.available_screens
-		curdir = dirname(__file__)
-		self.available_screens = [join(curdir, 'data', 'screens',
-			'{}.kv'.format(fn).lower()) for fn in self.available_screens]
-		self.go_mainmenu()
-	"""	
-	def go_previous_screen(self):
-		self.index = (self.index - 1) % len(self.available_screens)
-		screen = self.load_screen(self.index)
-		sm = self.root.ids.sm
-		sm.switch_to(screen, direction='right')
-		self.current_title = screen.name
-		#self.update_sourcecode()
-	
-	def go_next_screen(self):
-		self.index = (self.index + 1) % len(self.available_screens)
-		screen = self.load_screen(self.index)
-		sm = self.root.ids.sm
-		sm.switch_to(screen, direction='left')
-		self.current_title = screen.name
-		#self.update_sourcecode()
-		"""
-		
-	def go_mainmenu(self):
-		self.index = 0 % len(self.available_screens)
-		screen = self.load_screen(self.index)
-		sm = self.root.ids.sm
-		sm.switch_to(screen, direction='right')
-		self.current_title = screen.name
-	
-	def go_levelselect(self):
-		if(self.index!=0): 
-			side = 'up'
-		else: 
-			side = 'left'
-		self.index = 1 % len(self.available_screens)
-		screen = self.load_screen(self.index)
-		sm = self.root.ids.sm
-		sm.switch_to(screen, direction = side)
-		self.current_title = screen.name
-		
-	def go_level1(self):
-		self.index = 2 % len(self.available_screens)
-		screen = self.load_screen(self.index)
-		sm = self.root.ids.sm
-		sm.switch_to(screen, direction='down')
-		self.current_title = screen.name
-		
-	def load_screen(self, index):
-		if index in self.screens:
-			return self.screens[index]
-		screen = Builder.load_file(self.available_screens[index])
-		self.screens[index] = screen
-		return screen
-		
-	def _update_clock(self, dt):
-		self.time = time()
-	
 
- 
-LogicGateApp().run()
+class PongGame(Widget):
+    player1 = ObjectProperty(None)
+
+    def on_touch_move(self, touch):
+        if (touch.x > self.player1.x) & (touch.y > self.player1.y) & (touch.x < self.player1.x + self.player1.size[0]) & (touch.y < self.player1.y + self.player1.size[1]):
+            self.player1.center_y = touch.y
+            self.player1.center_x = touch.x
+
+class PongApp(App):
+    def build(self):
+        game = PongGame()
+        return game
+
+
+if __name__ == '__main__':
+    PongApp().run()
