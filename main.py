b18407e204deb3db6c5255aadc33c439de6cce42
Surdjak <n64player@live.com>
2017-11-28 19:03:00 +0100

    Commentaires (qui font bug, va savoir pourquoi)

--- main.py
+++ main.py
@@ -9,32 +9,37 @@ from kivy.vector import Vector
 from kivy.clock import Clock
 
 
+
+
+"""
+Classe basique d'une porte logique.
+Attributs:
+	-selected: indique si l'objet est en train d'etre déplacé
+	-outx: coordonnées x de la fin du fil de sortie
+	-outy: coordonées y de la fin du fil de sortie
+"""
 class Gate(Widget):
+
     selected = BooleanProperty(0)
     outx = NumericProperty(0)
     outy = NumericProperty(0)
-    
-#class Wire(Widget):
-#	sourceGate = Gate()
-#	targetGate = Gate()
-#
-#	def __init__(self, SG, TG):
-#		self.sourceGate = SG
-#		self.targetGate = TG
-    
+
+
+
+
+
+"""
+Classe définissant les évènements qui constituent le fonctionnement du jeu
+Attributs:
+	-gateList: Liste des portes actuellement en jeu
+"""
 class LGGame(Widget):
 	gateList = []
-	#wiring = False
-
-
+	
+	#Evenement se déclenchant au toucher de l'utilisateur sur l'écran ou au clic de la souris
+	#Permet la selection et le déplacement des portes
 	def on_touch_move(self, touch):
 		gate_grab = False
-		
-		#for gate in self.gateList:
-		#	if(touch.x > gate.x+180) & (touch.y > gate.y+80) & (touch.x < gate.x+180 + gate.outSize[0]) & (touch.y < gate.y+80 + gate.outSize[1]):
-		#		self.wiring = True
-		#		new_wire(self, gate)
-		#		return
 		
 		for gate in self.gateList:
 			if(gate.selected) & (touch.x > gate.x) & (touch.y > gate.y) & (touch.x < gate.x + gate.size[0]) & (touch.y < gate.y + gate.size[1]):
@@ -53,25 +58,33 @@ class LGGame(Widget):
 					gate.selected = True
 					gate_grab = True
 					return
-					
-					            
+			
+	#Methode de création d'une nouvelle porte, appelé par le bouton correspondant dans l'interface
 	def new_gate(self):
 		gate = Gate()
 		self.add_widget(gate)
 		self.gateList.append(gate)
-		
-	#def new_wire(self, SG, TG):
-	#	wire = Wire(self,SG,TG)
-	#	self.add_widget(wire)
-	#	self.wireList.append(wire)
-	
 
+
+
+
+
+"""
+Classe définissant l'application, elle représente le coeur du programme
+Attributs:
+	-game: Le jeu lui même, qui sera par la suite récupéré par le processus de Kivy
+"""
 class LGApp(App):
 	game = Widget()
+	
+	#Builder du jeu
 	def build(self):
 		self.game = LGGame()
 		return self.game
 
 
+
+
+#Lancement du jeu
 if __name__ == '__main__':
     LGApp().run()
